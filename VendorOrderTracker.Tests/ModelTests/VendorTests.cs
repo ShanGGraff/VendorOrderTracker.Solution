2170d6a66e7c6017173e6f9915c3ead3184db452
Shane Graff <copellius@gmail.com>
2021-09-30 14:40:46 -0700

    Add test for returning name and description values from Vendor class

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,13 @@ using System;
 namespace VendorOrderTracker.Tests
 {
   [TestClass]
-  public class VendorTests : IDisposable
+  public class VendorTests //: IDisposable
   {
 
-    public void Dispose()
-    {
-      Vendor.ClearAll();
-    }
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
   
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
@@ -21,6 +21,23 @@ namespace VendorOrderTracker.Tests
       string vendorDescription = "East location";
       Vendor newVendor = new Vendor(vendorName, vendorDescription);
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsNameDescription_String()
+    {
+      //Arrange
+      string name = "Darkstar.";
+      string description = "We sell the best bread ever";
+
+      //Act
+      Vendor newVendor = new Vendor(name, description);
+      string nameResult = newVendor.Name;
+      string descriptionResult = newVendor.Description;
+
+      //Assert
+      Assert.AreEqual(name, nameResult);
+      Assert.AreEqual(description, descriptionResult);
     }
 
     // [TestMethod]
